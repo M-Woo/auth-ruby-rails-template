ed75ab7c09248b698233443b0bfec8771c445066
m-woo <mark.chi.woo@gmail.com>
2017-04-06 08:07:15 -0700

    signup + navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  # get 'user/index'
+
+  # get 'user/create'
+
+  # get 'user/edit'
+
+  # get 'user/update'
+
+  # get 'user/new'
+
+  # get 'user/destroy'
+
+  # get 'user/show'
+
 	root 'main#index'
 	
   # get 'main/index'
@@ -6,6 +20,8 @@ Rails.application.routes.draw do
  	get "login" => 'sessions#new'
  	post "login" => 'sessions#create'
  	get "logout" => 'sessions#destroy'
+
+ 	resources :user
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
